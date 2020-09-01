46dd9efd621621883fe254b02303524600218703
maciek <maciej.b.lukasik@gmail.com>
2020-09-01 15:40:30 +0200

    feat(todo): Add rest api data source for todos
    Add service to manage http calls to todo's rest api and proxy configuration for angular

---
+++ libs/todo/data-access/src/lib/services/todo-rest.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Observable } from 'rxjs';
+import { TodosEntity } from '../+state/todos.models';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class TodoRestService extends TodoDataService {
+
+  private readonly resourceUrl = 'todos';
+  private readonly headers = {'Content-Type': 'application/json'};
+
+  constructor(private http: HttpClient) {
+    super();
+  }
+
+  getAllTodos(): Observable<TodosEntity[]> {
+      return this.http.get<TodosEntity[]>(this.resourceUrl);
+  }
+  addTodo(newTodo: TodosEntity) {
+      return this.http.post<any>(this.resourceUrl, newTodo, {headers: this.headers});
+  }
+  editTodo(entity: TodosEntity, patch: Partial<TodosEntity>): Observable<any> {
+    return this.http.patch<any>(`${this.resourceUrl}/${entity.id}`, patch, {headers: this.headers});
+  }
+  removeTodo(entity: TodosEntity): Observable<any> {
+    return this.http.delete<any>(`${this.resourceUrl}/${entity.id}`, {headers: this.headers});
+  }
+}
