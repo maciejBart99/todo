d1965258652f489d2967403d809c86b37b7a76c1
maciek <maciej.b.lukasik@gmail.com>
2020-09-01 11:07:43 +0200

    init

--- libs/todo/data-access/src/lib/services/todo-local-storage.service.ts
+++ libs/todo/data-access/src/lib/services/todo-local-storage.service.ts
@@ -4,50 +4,52 @@ import { filter, map, switchMap, tap, toArray } from 'rxjs/operators';
 import { TodosEntity } from '@todo-application/todo/data-access';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoLocalStorageService {
-
   private readonly featureKey = 'todos';
 
-  constructor() { }
-
   getAllTodos(): Observable<TodosEntity[]> {
-      return of(localStorage.getItem(this.featureKey)).pipe(
-      map(ser => {
-        return ser ? JSON.parse(ser) as TodosEntity[] : [];
-      }));
+    return of(localStorage.getItem(this.featureKey)).pipe(
+      map((ser) => {
+        return ser ? (JSON.parse(ser) as TodosEntity[]) : [];
+      })
+    );
   }
 
   addTodo(newTodo: TodosEntity): Observable<any> {
     return this.getAllTodos().pipe(
-      map(all => {
+      map((all) => {
         all.push(newTodo);
         return all;
       }),
-      tap(all => this.saveTodos(all))
+      tap((all) => this.saveTodos(all))
     );
   }
 
   editTodo(entity: TodosEntity, patch: Partial<TodosEntity>): Observable<any> {
     return this.getAllTodos().pipe(
-      map(all => [...(all.filter(todo => todo.id !== entity.id)), {...entity, ...patch}]),
-      tap(all => this.saveTodos(all))
-    )
+      map((all) => [
+        ...all.filter((todo) => todo.id !== entity.id),
+        { ...entity, ...patch },
+      ]),
+      tap((all) => this.saveTodos(all))
+    );
   }
 
   removeTodo(entity: TodosEntity): Observable<any> {
     return this.getAllTodos().pipe(
-      switchMap(all => from(all).pipe(
-        filter(item => item.id !== entity.id),
-        toArray()
-      )),
-      tap(all => this.saveTodos(all))
-    )
+      switchMap((all) =>
+        from(all).pipe(
+          filter((item) => item.id !== entity.id),
+          toArray()
+        )
+      ),
+      tap((all) => this.saveTodos(all))
+    );
   }
 
   private saveTodos(todos: TodosEntity[]): void {
     localStorage.setItem(this.featureKey, JSON.stringify(todos));
   }
-
 }
