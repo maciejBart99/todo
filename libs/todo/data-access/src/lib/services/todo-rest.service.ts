cc4d6617bde3231e989b94e250a3ebd937810016
maciek <maciej.b.lukasik@gmail.com>
2020-09-02 09:08:05 +0200

    refactor(todo): Remove unnecesary headers and request response typing
    Remove explicit application/json header from todo rest api service
    Add typings to todod rest api service

--- libs/todo/data-access/src/lib/services/todo-rest.service.ts
+++ libs/todo/data-access/src/lib/services/todo-rest.service.ts
@@ -3,12 +3,12 @@ import { TodoDataService } from './todo-data.service';
 import { Observable } from 'rxjs';
 import { TodosEntity } from '../+state/todos.models';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class TodoRestService implements TodoDataService {
 
   private readonly resourceUrl = 'todos';
-  private readonly headers = {'Content-Type': 'application/json'};
 
   constructor(private http: HttpClient) {}
 
@@ -16,12 +16,12 @@ export class TodoRestService implements TodoDataService {
       return this.http.get<TodosEntity[]>(this.resourceUrl);
   }
   addTodo(newTodo: TodosEntity) {
-      return this.http.post<any>(this.resourceUrl, newTodo, {headers: this.headers});
+      return this.http.post<TodosEntity>(this.resourceUrl, newTodo);
   }
   editTodo(entity: TodosEntity, patch: Partial<TodosEntity>): Observable<any> {
-    return this.http.patch<any>(`${this.resourceUrl}/${entity.id}`, patch, {headers: this.headers});
+    return this.http.patch<TodosEntity>(`${this.resourceUrl}/${entity.id}`, patch);
   }
   removeTodo(entity: TodosEntity): Observable<any> {
-    return this.http.delete<any>(`${this.resourceUrl}/${entity.id}`, {headers: this.headers});
+    return this.http.delete<{}>(`${this.resourceUrl}/${entity.id}`);
   }
 }
